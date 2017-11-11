d7cd8ff442972a23277aac24fb0aa5bfec7d760c
Home\voldelord <r.f.niel@student.rug.nl>
2017-11-11 11:49:13 +0100

    Behaviours get an inputKey at creation, this input key determines which input variables from the total states are used by the network.

--- src/AI/behaviour.java
+++ src/AI/behaviour.java
@@ -7,20 +7,29 @@ import java.util.Queue;
 import Neural.NeuralNetwork;
 
 public class behaviour {
-	final double gamma=0.98;
+	final double gamma = 0.98;
 	NeuralNetwork n;
 	int[] rewardWeights;
-	//ArrayList<double[]> inputs;
+	int[] inputKey;
 	Queue<double[]> inputs;
 	Queue<Integer> action;
 	int[] size;
 
-	public behaviour(int[] size, int[] rewardWeights) {
+	public behaviour(int[] size, int[] rewardWeights, int[] inputKey) {
 		n = new NeuralNetwork(size);
-		this.rewardWeights = rewardWeights;
-		inputs  = new LinkedList<double[]>();
-		this.size=size;
-		action=new LinkedList<Integer>();
+		this.rewardWeights = rewardWeights.clone();
+		inputs = new LinkedList<double[]>();
+		this.inputKey = inputKey.clone();
+		this.size = size;
+		action = new LinkedList<Integer>();
+	}
+
+	private double[] decodeInput(double[] rawInput) {
+		double[] input = new double[inputKey.length];
+		for (int i = 0; i < inputKey.length; i++) {
+			input[i]=rawInput[inputKey[i]];
+		}
+		return input;
 	}
 
 	public void updateNetwork(int state, int[] rewards) {
@@ -29,37 +38,35 @@ public class behaviour {
 		for (int i = 0; i < rewards.length; i++) {
 			reward += rewardWeights[i] * rewards[i];
 		}
-		
+
 		double[][] activation;
 		double[] expectedOutput;
 		double[] nextExpected;
-		double[] input=inputs.poll();
+		double[] input = inputs.poll();
 		double max;
-		
+
 		if (inputs.size() > 0) {
-			activation=n.forwardProp(inputs.peek());
-			nextExpected=activation[size.length-1].clone();
+			activation = n.forwardProp(decodeInput(inputs.peek()));
+			nextExpected = activation[size.length - 1].clone();
 			max = Double.NEGATIVE_INFINITY;
 			for (int j = 0; j < nextExpected.length; j++) {
-				//String str = String.format("%1.2f", nextExpected[j]) + " ";
+				// String str = String.format("%1.2f", nextExpected[j]) + " ";
 				// System.out.print(nextExpected[j]);
 				if (nextExpected[j] > max) {
 					max = nextExpected[j];
 				}
 			}
 
-		}else{
-			max=0;
+		} else {
+			max = 0;
 		}
-		
-		activation=n.forwardProp(input);
-		expectedOutput=activation[size.length-1].clone();
-		expectedOutput[action.poll()]=reward+gamma*max;
-		
-		n.backProp(activation,expectedOutput);
-		
-		
-		
+
+		activation = n.forwardProp(input);
+		expectedOutput = activation[size.length - 1].clone();
+		expectedOutput[action.poll()] = reward + gamma * max;
+
+		n.backProp(activation, expectedOutput);
+
 	}
 
 }
